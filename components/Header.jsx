7ed0000c9091ad3c3603dfdb0fac71b19a34f36d
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-05-29 16:03:17 -0700

    Added functional categories section. Added comment feature. Added section to display all previous comments.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,41 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Link from 'next/link';
 
-const categories = [{name: 'Setup', slug: 'setup' }, {name: 'Serato', slug: 'serato'}]
+import { getCategories } from '../services'
+
 
 const Header = () => {
-  return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='border-b w-full inline-block border-blue-400 py-8'>
-            <div className='md:float-left block'>
-                <Link href="/">
-                    <span className='cursor-pointer font-bold text-4xl text-black'>
-                        DJ Blog
-                    </span>
-                </Link>
-            </div>
-            <div className='hidden md:float-left md:contents'>
-                {categories.map((category) => (
-                    <Link key={category.slug} href={`/category/${category.slug}`} >
-                        <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
-                            {category.name}
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, []);
+
+    return (
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='border-b w-full inline-block border-blue-400 py-8'>
+                <div className='md:float-left block'>
+                    <Link href="/">
+                        <span className='cursor-pointer font-bold text-4xl text-black'>
+                            DJ Blog
                         </span>
                     </Link>
-                ))}
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`} >
+                            <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
